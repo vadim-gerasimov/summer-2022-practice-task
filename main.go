f3c2406f498861d1359742ed820d6fc73623a69a
Vadim Gerasimov <gerasmeb34@gmail.com>
2022-08-10 14:09:07 +0300

    task solution v3

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"sort"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -26,40 +25,42 @@ type Train struct {
 func (t Trains) findBy(depID, arrID int) Trains {
 	var found Trains
 	for _, train := range t {
-		depOk := train.DepartureStationID == depID
-		arrOk := train.ArrivalStationID == arrID
-		if depOk && arrOk {
+		if train.DepartureStationID == depID && train.ArrivalStationID == arrID {
 			found = append(found, train)
 		}
 	}
+
 	return found
 }
 
-var sortCriterias = []string{
-	0: "price",
-	1: "arrival-time",
-	2: "departure-time",
-}
+const (
+	sortCriteriaPrice = "price"
+	sortCriteriaArr   = "arrival-time"
+	sortCriteriaDep   = "departure-time"
+)
 
-func (t Trains) sortBy(criteria string) Trains {
+func (t Trains) sortBy(criteria string) (Trains, error) {
 	sorted := make(Trains, len(t))
 	copy(sorted, t)
+
 	switch criteria {
-	case sortCriterias[0]:
+	case sortCriteriaPrice:
 		sort.SliceStable(sorted, func(i, j int) bool {
 			return sorted[i].Price < sorted[j].Price
 		})
-	case sortCriterias[1]:
+	case sortCriteriaArr:
 		sort.SliceStable(sorted, func(i, j int) bool {
 			return sorted[i].ArrivalTime.UnixNano() < sorted[j].ArrivalTime.UnixNano()
 		})
-	case sortCriterias[2]:
+	case sortCriteriaDep:
 		sort.SliceStable(sorted, func(i, j int) bool {
 			return sorted[i].DepartureTime.UnixNano() < sorted[j].DepartureTime.UnixNano()
 		})
 	default:
+		return nil, errBadCriteria
 	}
-	return sorted
+
+	return sorted, nil
 }
 
 func main() {
@@ -67,26 +68,56 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for _, v := range result {
 		fmt.Printf("%+v\n", v)
 	}
 }
 
-func getQuery() (dep, arr, by string) {
+func getQuery() (dep, arr, criteria string) {
 	fmt.Print("Departure station number: ")
 	fmt.Scanf("%s", &dep)
+
 	fmt.Print("Arrival station number: ")
 	fmt.Scanf("%s", &arr)
-	var validBy = strings.Join(sortCriterias, "|")
-	fmt.Print("Criteria (" + validBy + "): ")
-	fmt.Scanf("%s", &by)
+
+	fmt.Print("Criteria: ")
+	fmt.Scanf("%s", &criteria)
+
 	return
 }
 
+var (
+	errEmptyDep = errors.New("empty departure station")
+	errEmptyArr = errors.New("empty arrival station")
+
+	errBadDep = errors.New("bad departure station input")
+	errBadArr = errors.New("bad arrival station input")
+
+	errBadCriteria = errors.New("unsupported criteria")
+)
+
 func FindTrains(dep, arr, criteria string) (Trains, error) {
-	depID, arrID, err := validateQuery(dep, arr, criteria)
-	if err != nil {
-		return nil, err
+	if dep == "" {
+		return nil, errEmptyDep
+	}
+	depID, err := strconv.Atoi(dep)
+	if err != nil || depID < 0 {
+		return nil, errBadDep
+	}
+
+	if arr == "" {
+		return nil, errEmptyArr
+	}
+	arrID, err := strconv.Atoi(arr)
+	if err != nil || arrID < 0 {
+		return nil, errBadArr
+	}
+
+	switch criteria {
+	case sortCriteriaPrice, sortCriteriaArr, sortCriteriaDep:
+	default:
+		return nil, errBadCriteria
 	}
 
 	trains, err := getTrains()
@@ -94,90 +125,85 @@ func FindTrains(dep, arr, criteria string) (Trains, error) {
 		return nil, err
 	}
 
-	foundTrains := trains.findBy(depID, arrID).sortBy(criteria)
+	foundTrains, err := trains.findBy(depID, arrID).sortBy(criteria)
+	if err != nil {
+		return nil, err
+	}
+
 	if len(foundTrains) == 0 {
 		return nil, nil
 	}
 	if len(foundTrains) > 3 {
 		foundTrains = foundTrains[:3]
 	}
+
 	return foundTrains, nil
 }
 
-var ErrEmptyDep = errors.New("empty departure station")
-var ErrEmptyArr = errors.New("empty arrival station")
+const (
+	parseTime = "15:04:05"
+	dataPath  = "data.json"
+)
 
-var ErrBadDep = errors.New("bad departure station input")
-var ErrBadArr = errors.New("bad arrival station input")
-
-var ErrBadBy = errors.New("unsupported criteria")
-
-func validateQuery(dep, arr, by string) (depID, arrID int, err error) {
-	if dep == "" {
-		return 0, 0, ErrEmptyDep
-	}
-	depID, err = strconv.Atoi(dep)
-	if err != nil || depID < 0 {
-		return 0, 0, ErrBadDep
-	}
-
-	if arr == "" {
-		return 0, 0, ErrEmptyArr
-	}
-	arrID, err = strconv.Atoi(arr)
-	if err != nil || arrID < 0 {
-		return 0, 0, ErrBadArr
-	}
-
-	valid := false
-	for _, c := range sortCriterias {
-		if by == c {
-			valid = true
-		}
-	}
-	if !valid {
-		return 0, 0, ErrBadBy
-	}
-	return depID, arrID, nil
-}
-
-const ParseTime = "15:04:05"
-
-var dataPath = "data.json"
+var errTypeAssertion = errors.New("invalid type assertion")
 
 func getTrains() (Trains, error) {
 	marshaled, err := os.ReadFile(dataPath)
 	if err != nil {
 		return nil, err
 	}
+
 	var unmarshaled []map[string]any
 	if err := json.Unmarshal(marshaled, &unmarshaled); err != nil {
 		return nil, err
 	}
+
 	var trains Trains
 	for _, u := range unmarshaled {
 		var t Train
 
-		trainID := u["trainId"].(float64)
+		trainID, ok := u["trainId"].(float64)
+		if !ok {
+			return nil, errTypeAssertion
+		}
 		t.TrainID = int(trainID)
 
-		departureStationID := u["departureStationId"].(float64)
+		departureStationID, ok := u["departureStationId"].(float64)
+		if !ok {
+			return nil, errTypeAssertion
+		}
 		t.DepartureStationID = int(departureStationID)
 
-		arrivalStationID := u["arrivalStationId"].(float64)
+		arrivalStationID, ok := u["arrivalStationId"].(float64)
+		if !ok {
+			return nil, errTypeAssertion
+		}
 		t.ArrivalStationID = int(arrivalStationID)
 
-		price := u["price"].(float64)
+		price, ok := u["price"].(float64)
+		if !ok {
+			return nil, errTypeAssertion
+		}
 		t.Price = float32(price)
 
-		if t.ArrivalTime, err = time.Parse(ParseTime, u["arrivalTime"].(string)); err != nil {
+		arrTime, ok := u["arrivalTime"].(string)
+		if !ok {
+			return nil, errTypeAssertion
+		}
+		if t.ArrivalTime, err = time.Parse(parseTime, arrTime); err != nil {
 			return nil, err
 		}
 
-		if t.DepartureTime, err = time.Parse(ParseTime, u["departureTime"].(string)); err != nil {
+		depTime, ok := u["departureTime"].(string)
+		if !ok {
+			return nil, errTypeAssertion
+		}
+		if t.DepartureTime, err = time.Parse(parseTime, depTime); err != nil {
 			return nil, err
 		}
+
 		trains = append(trains, t)
 	}
+
 	return trains, nil
 }
